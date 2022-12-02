5fd2b8d677e2c6bc4c3cd5eec0893d53922e761f
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2022-12-02 14:36:23 -0500

    Rearranged Day2 solution functions/grouping

--- untitled/src/main/kotlin/Day2.kt
+++ untitled/src/main/kotlin/Day2.kt
@@ -5,21 +5,6 @@ fun rpsScore(round: String): Int {
     return roundScore(rpsPoint(yourMove), rpsPoint(opponentMove))
 }
 
-/*
-you   1  1  2  2  3  3     1 - rock
-opp   2  3  1  3  1  2     2 - paper
-dif  -1 -2  1 -1  2  1     3 - scissors
-+3%3  2  1  1  2  2  1
-      L  W  W  L  L  W
- */
-
-private val pointsFor = mapOf("A" to 1, "X" to 1, "B" to 2, "Y" to 2, "C" to 3, "Z" to 3)
-fun rpsPoint(move: String) = pointsFor[move] ?: 0
-
-private val score = arrayOf(3, 6, 0)
-private fun roundScore(you: Int, opp: Int) = you + score[result(you, opp)]
-private fun result(you: Int, opp: Int) = ((you - opp) + 3) % 3
-
 fun day2Part2(input: List<String>) = input.sumOf { rpsScore2(it) }.toString()
 
 fun rpsScore2(round: String): Int {
@@ -29,12 +14,27 @@ fun rpsScore2(round: String): Int {
     return roundScore(youPt, oppPt)
 }
 
-private val defeat = arrayOf(0, 2, 3, 1)
-private val loseTo = arrayOf(0, 3, 1, 2)
+fun rpsPoint(move: String) = pointsFor[move] ?: 0
+private val pointsFor = mapOf("A" to 1, "X" to 1, "B" to 2, "Y" to 2, "C" to 3, "Z" to 3)
+
+private fun roundScore(you: Int, opp: Int) = you + score[result(you, opp)]
+private val score = arrayOf(3, 6, 0)
+
+/*
+you   1  1  2  2  3  3     1 - rock
+opp   2  3  1  3  1  2     2 - paper
+dif  -1 -2  1 -1  2  1     3 - scissors
++3%3  2  1  1  2  2  1
+      L  W  W  L  L  W
+ */
+private fun result(you: Int, opp: Int) = ((you - opp) + 3) % 3
 
 private const val DRAW = 0
 private const val WIN = 1
 private const val LOSE = 2
+
+private val defeat = arrayOf(0, 2, 3, 1)
+private val loseTo = arrayOf(0, 3, 1, 2)
 
 fun strategicPoint(opponent: Int, scoreWanted: Int) =
     when (scoreWanted) {
