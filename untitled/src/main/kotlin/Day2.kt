dd261d3553274b48a7924942dd1c5c0db422d98a
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2022-12-02 14:22:29 -0500

    Refactored Day2 solution

--- untitled/src/main/kotlin/Day2.kt
+++ untitled/src/main/kotlin/Day2.kt
@@ -1,57 +1,51 @@
 fun day2Part1(input: List<String>) = input.sumOf { rpsScore(it) }.toString()
 
-fun rpsScore(round: String) : Int {
-    val (opponent, you) = round.split(" ")
-    return score(rpsPoint(you), rpsPoint(opponent))
+fun rpsScore(round: String): Int {
+    val (opponentMove, yourMove) = round.split(" ")
+    return roundScore(rpsPoint(yourMove), rpsPoint(opponentMove))
 }
 
 /*
-you  1  1  2  2  3  3
-opp  2  3  1  3  1  2
-    -1 -2  1 -1  2  1
-+3%3 2  1  1  2  2  1
-     L  W  W  L  L  W
+you   1  1  2  2  3  3     1 - rock
+opp   2  3  1  3  1  2     2 - paper
+dif  -1 -2  1 -1  2  1     3 - scissors
++3%3  2  1  1  2  2  1
+      L  W  W  L  L  W
  */
 
-fun rpsPoint(move : String) =
-when (move) {
-    "A", "X" -> 1 // rock
-    "B", "Y" -> 2 // paper
-    "C", "Z" -> 3 // scissors
-    else -> 0
-}
+private val pointsFor = mapOf("A" to 1, "X" to 1, "B" to 2, "Y" to 2, "C" to 3, "Z" to 3)
+fun rpsPoint(move: String) = pointsFor[move] ?: 0
 
-private fun score(you: Int, opp: Int) =
-    you + when (winner(you, opp)) {
-        0 -> 3
-        1 -> 6
-        else -> 0
-    }
-
-private fun winner(youPt: Int, oppPt: Int) = ((youPt - oppPt) + 3) % 3
+private val score = arrayOf(3, 6, 0)
+private fun roundScore(you: Int, opp: Int) = you + score[result(you, opp)]
+private fun result(you: Int, opp: Int) = ((you - opp) + 3) % 3
 
 fun day2Part2(input: List<String>) = input.sumOf { rpsScore2(it) }.toString()
 
 fun rpsScore2(round: String): Int {
-    val (opponent, strategy) = round.split(" ")
-    val oppPt = rpsPoint(opponent)
-    val youPt = strategicPoint(oppPt, targetScore(strategy))
-    return score(youPt, oppPt)
+    val (opponentMove, strategy) = round.split(" ")
+    val oppPt = rpsPoint(opponentMove)
+    val youPt = strategicPoint(oppPt, scoreWanted(strategy))
+    return roundScore(youPt, oppPt)
 }
 
 private val defeat = arrayOf(0, 2, 3, 1)
 private val loseTo = arrayOf(0, 3, 1, 2)
 
-fun strategicPoint(oppPt: Int, targetScore: Int) =
-when (targetScore) {
-    0 -> oppPt
-    1 -> defeat[oppPt]
-    else -> loseTo[oppPt]
-}
+private const val DRAW = 0
+private const val WIN = 1
+private const val LOSE = 2
 
-private fun targetScore(code: String) =
-when (code) {
-    "X" -> 2
-    "Y" -> 0
-    else -> 1
-}
+fun strategicPoint(opponent: Int, scoreWanted: Int) =
+    when (scoreWanted) {
+        DRAW -> opponent
+        WIN -> defeat[opponent]
+        else -> loseTo[opponent]
+    }
+
+private fun scoreWanted(strategy: String) =
+    when (strategy) {
+        "X" -> LOSE
+        "Y" -> DRAW
+        else -> WIN
+    }
