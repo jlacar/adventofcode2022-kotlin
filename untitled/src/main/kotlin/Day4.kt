505c90c534fce4cb9f437d246967fdaed493ea4b
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2022-12-03 07:15:13 -0500

    Enhanced indicators for solution status

--- untitled/src/main/kotlin/Day4.kt
+++ untitled/src/main/kotlin/Day4.kt
@@ -1,2 +1,3 @@
-class Day4 {
-}
+fun day4Part1(input: List<String>) = 6
+
+fun day4Part2(input: List<String>) = -1
--- untitled/src/main/kotlin/Main.kt
+++ untitled/src/main/kotlin/Main.kt
@@ -6,7 +6,7 @@ fun main(args: Array<String>) {
     solve(1, { day1Part1(it) }, { day1Part2(it) }, "Day1-sample.txt|24000|45000", "Day1.txt|71502|208191")
     solve(2, { day2Part1(it) }, { day2Part2(it) }, "Day2-sample.txt|15|12", "Day2.txt|14264|12382")
     solve(3, { day3Part1(it) }, { day3Part2(it) }, "Day3-sample.txt|157|70", "Day3.txt|7446|2646")
-    solve(4, notYetImplemented, notYetImplemented /*, "Day4-sample.txt|1|1", "Day4.txt|1|1" */)
+    solve(4, { day4Part1(it) }, notYetImplemented, "Day4-sample.txt|1|1" /*, "Day4.txt|1|1" */)
 //    solve(5, notYetImplemented, notYetImplemented /*, "Day5-sample.txt|1|1", "Day5.txt|1|1" */)
 //    solve(6, notYetImplemented, notYetImplemented /*, "Day6-sample.txt|1|1", "Day6.txt|1|1" */)
 //    solve(7, notYetImplemented, notYetImplemented /*, "Day7-sample.txt|1|1", "Day7.txt|1|1" */)
@@ -41,11 +41,15 @@ private fun solve(n: Int, part1: Solution, part2: Solution, vararg params: Strin
     }
 }
 
-private fun result(n: Int, result: Int, expected: Int) = 
-    println(" Part $n: $result ${mark(expected, result)}")
+private fun result(n: Int, actual: Int, expected: Int) =
+    println(" Part $n: ${mark(expected, actual)}")
 
-private fun mark(expected: Int, actual: Int) = 
-    if (actual == expected) "✅" else "❌- expected [$expected]"
+private fun mark(expected: Int, actual: Int) = when (actual) {
+    -1 -> "❗(pending...)"
+    0 -> "⭐(working on it...)"
+    else -> if (actual == expected) "$actual ✅"
+            else "❌expected [$expected] but got [$actual]"
+}
 
 class InputReader(private val fileName: String) {
     fun lines() = this::class.java.getResourceAsStream(fileName).bufferedReader().readLines()
