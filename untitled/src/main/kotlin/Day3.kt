251b26be705b04bf9f8bdd115e67d0800f4931ec
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2022-12-03 02:43:15 -0500

    Solved Day3 Part 2, refactored Part 1 solution to tighten up code

--- untitled/src/main/kotlin/Day3.kt
+++ untitled/src/main/kotlin/Day3.kt
@@ -1,3 +1,5 @@
+import javax.swing.text.MutableAttributeSet
+
 fun day3Part1(input: List<String>) = input.sumOf { ruckPriority(it) }
 
 fun ruckPriority(line: String): Int {
@@ -5,11 +7,28 @@ fun ruckPriority(line: String): Int {
     return priority(commonItemIn(compartment1, compartment2))
 }
 
-fun commonItemIn(compartment1: String, compartment2: String): Char {
-    val items1 = HashSet(compartment1.toCharArray().toList())
-    val items2 = HashSet(compartment2.toCharArray().toList())
-    return items1.intersect(items2).first()
+fun day3Part2(input: List<String>) = sumOfBadgePriority(input)
+
+fun sumOfBadgePriority(input: List<String>): Int {
+    val group: MutableList<Set<Char>> = mutableListOf()
+    val priorities: MutableList<Int> = mutableListOf()
+    input.forEach { items ->
+        group.add(items.toSet())
+        if (group.size == 3) {
+            priorities.add(badgePriorityOf(group))
+            group.clear()
+        }
+    }
+    return priorities.sum()
 }
+
+fun badgePriorityOf(group: MutableList<Set<Char>>): Int {
+    val (elf1, elf2, elf3) = group
+    return priority(elf1.intersect(elf2).intersect(elf3).first())
+}
+
+fun commonItemIn(compartment1: String, compartment2: String) =
+    compartment1.toSet().intersect(compartment2.toSet()).first()
 
 fun halve(line: String): Pair<String, String> {
     val half = line.length / 2
@@ -18,5 +37,3 @@ fun halve(line: String): Pair<String, String> {
 
 fun priority(ch: Char) =
     ch.lowercaseChar() - 'a' + (if (ch.isLowerCase()) 1 else 27)
-
-fun day3Part2(input: List<String>) = 0
--- untitled/src/main/kotlin/Main.kt
+++ untitled/src/main/kotlin/Main.kt
@@ -5,7 +5,7 @@ fun main(args: Array<String>) {
 
     solve(1, { day1Part1(it) }, { day1Part2(it) }, "Day1-sample.txt|24000|45000", "Day1.txt|71502|208191")
     solve(2, { day2Part1(it) }, { day2Part2(it) }, "Day2-sample.txt|15|12", "Day2.txt|14264|12382")
-    solve(3, { day3Part1(it) }, notYetImplemented, "Day3-sample.txt|157|1", "Day3.txt|7446|1")
+    solve(3, { day3Part1(it) }, { day3Part2(it) }, "Day3-sample.txt|157|70", "Day3.txt|7446|2646")
     solve(4, notYetImplemented, notYetImplemented /*, "Day4-sample.txt|1|1", "Day4.txt|1|1" */)
     solve(5, notYetImplemented, notYetImplemented /*, "Day5-sample.txt|1|1", "Day5.txt|1|1" */)
     solve(6, notYetImplemented, notYetImplemented /*, "Day6-sample.txt|1|1", "Day6.txt|1|1" */)
