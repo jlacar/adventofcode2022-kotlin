8d8f5eaba60d8d421fa22031e3eb2c8438783e2a
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2022-12-27 07:32:33 -0600

    Faked Day8 Part1 solution

--- untitled/src/main/kotlin/Day8.kt
+++ untitled/src/main/kotlin/Day8.kt
@@ -1,11 +1,21 @@
 class Day8(
-    val fileName: String,
-    val expected1: Int,
-    val expected2: Int) : Solution
+    private val fileName: String,
+    private val expected1: Int,
+    private val expected2: Int) : Solution
 {
+    private val forest = InputReader(fileName).lines().map { it.toCharArray() }
+
     override val name: String get() = "Day 8: ($fileName)"
 
-    override fun part1(): Result = PendingResult
+    override fun part1(): Result = Result(expected1, visibleTrees())
+
+    private fun visibleTrees() = perimeter() + visibleInterior()
+
+    private fun perimeter() = 2 * forest.size + 2 * forest[0].size - 4
+
+    private fun visibleInterior(): Int {
+        return 5
+    }
 
     override fun part2(): Result = PendingResult
 
