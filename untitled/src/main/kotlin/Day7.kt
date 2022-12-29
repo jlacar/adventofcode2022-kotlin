7380f9615b29a016477ce4dfe1bec150597d0836
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2022-12-29 01:29:24 -0600

    Eliminated currentDir variable and replaced with currentDir() function

--- untitled/src/main/kotlin/Day7.kt
+++ untitled/src/main/kotlin/Day7.kt
@@ -17,38 +17,31 @@ class Day7  (
 
     private fun spaceNeeded() = 30_000_000 - (70_000_000 - root.size())
 
+    private fun findDirs(predicate: (FileAoC7) -> Boolean): List<FileAoC7> = mutableListOf<FileAoC7>()
+        .also { matches -> root.walkDirectories { if (predicate.invoke(it)) matches.add(it) } }
+
     private fun parse(input: List<String>): FileAoC7 {
         val root = FileAoC7("/")
-        var currentDir = root
-        val dirStack: Stack<FileAoC7> = Stack()
+        val dirStack = Stack<FileAoC7>()
 
         fun dirName(line: String) = line.split(" ")[1]
-
-        fun toFile(line: String) = line.split(" ").let { FileAoC7(it[1], it[0].toInt()) }
-
         fun toDir(line: String) = FileAoC7(dirName(line))
-
-        fun upDir() = if (dirStack.isNotEmpty()) dirStack.pop() else dirStack.push(root).let { root }
-
-        fun chdir(line: String) = dirStack.push(currentDir).let {
-            currentDir.contents.first { it.name == dirName(line.substring(2)) }
-        }
+        fun toFile(line: String) = line.split(" ").let { FileAoC7(it[1], it[0].toInt()) }
+        fun currentDir() = dirStack.peek()!!.contents
+        fun chdir(dir: String) = currentDir().first { it.name == dir }.also { dirStack.push(it) }
 
         input.forEach { line ->
             when {
-                line.startsWith("$ cd /") -> currentDir = root.also { dirStack.clear() }
-                line.startsWith("$ cd ..") -> currentDir = upDir()
-                line.startsWith("$ cd ") -> currentDir = chdir(line)
-                line.startsWith("dir ") -> currentDir.contents.add(toDir(line.trim()))
-                line.first().isDigit() -> currentDir.contents.add(toFile(line.trim()))
+                line.startsWith("$ cd /") -> dirStack.push(root)
+                line.startsWith("$ cd ..") -> dirStack.pop()
+                line.startsWith("$ cd ") -> chdir(dirName(line.substring(2)))
+                line.startsWith("dir ") -> currentDir().add(toDir(line))
+                line.first().isDigit() -> currentDir().add(toFile(line))
             }
         }
 
         return root
     }
-
-    private fun findDirs(predicate: (FileAoC7) -> Boolean): List<FileAoC7> = mutableListOf<FileAoC7>()
-        .also { matches -> root.walkDirectories { if (predicate.invoke(it)) matches.add(it) } }
 }
 
 data class FileAoC7 (val name: String, val bytes: Int, val isDirectory: Boolean = false) {
