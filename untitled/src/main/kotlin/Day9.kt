f2660b1ac8a14db6e038fda941ff24b75de7976f
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2023-01-04 06:17:39 -0500

    Implemented parsing of Day9 input, framed Part1 solution

--- untitled/src/main/kotlin/Day9.kt
+++ untitled/src/main/kotlin/Day9.kt
@@ -8,9 +8,39 @@ class Day9(
     override val day get() = 9
     override val source get() = "$fileName"
 
-    override fun part1() = PendingResult
+    override fun part1() = Result(expected1, positionsVisited().size)
+
+    val moves = parse(InputReader(fileName).lines())
+
+    private fun positionsVisited() =
+        mutableSetOf<Pair<Int, Int>>().let { visited ->
+            with (visited) {
+                add(Pair(0, 0))
+                add(Pair(1, 0))
+                add(Pair(0, 1))
+                add(Pair(0, 0))
+            }
+            visited
+        }
 
     override fun part2() = PendingResult
+
+    private fun parse(lines: List<String>): List<Pair<Int, Int>> {
+
+        fun parseMove(move: String): Pair<Int, Int> {
+            val (direction, n) = move.split(" ")
+            val steps = n.toInt()
+            return when (direction) {
+                "R" -> Pair(steps, 0)
+                "L" -> Pair(-steps, 0)
+                "U" -> Pair(0, steps)
+                "D" -> Pair(0, -steps)
+                else -> Pair(0, 0)
+            }
+        }
+
+        return lines.map { parseMove(it) }
+    }
 }
 
 fun main() {
