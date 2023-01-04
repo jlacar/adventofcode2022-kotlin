75d17c376aef74ddb92c12e95c44541a71895df0
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2023-01-04 05:33:20 -0500

    Prepped for solving Day9

---
+++ untitled/src/main/kotlin/Day9.kt
@@ -0,0 +1,20 @@
+import kotlin.Result
+
+class Day9(
+    private val fileName: String,
+    private val expected1: Int,
+    private val expected2: Int) : Solution
+{
+    override val day get() = 8
+    override val source get() = "$fileName"
+
+    override fun part1() = PendingResult
+
+    override fun part2() = PendingResult
+}
+
+fun main() {
+    Solution.report(
+        Day9("Day9-sample.txt", 0, 0),
+    )
+}
