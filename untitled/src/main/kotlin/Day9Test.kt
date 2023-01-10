bc0960e6f8f88041fadfda0b77dacd00e328169d
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2023-01-09 23:28:23 -0500

    Reformatted Day9Test

--- untitled/src/main/kotlin/Day9Test.kt
+++ untitled/src/main/kotlin/Day9Test.kt
@@ -36,12 +36,14 @@ class Day9Test {
         inner class `distanceTo(Position)` {
 
             @ParameterizedTest(name = "{0} step from origin, expect ({1}, {2})")
-            @CsvSource(delimiter = ',', textBlock =
-            """ R, -1, 0
+            @CsvSource(
+                delimiter = ',', textBlock =
+                """ R, -1, 0
                 L, 1, 0
                 U, 0, -1
                 D, 0, 1
-                NONE, 0, 0""")
+                NONE, 0, 0"""
+            )
             fun `to origin from relative position `(dir: Direction, xE: Int, yE: Int) {
                 assertEquals(Position(xE, yE), origin.step(dir).distanceTo(origin))
             }
@@ -50,25 +52,29 @@ class Day9Test {
         @Nested
         inner class `stepToward(Position)` {
             @ParameterizedTest(name = "from ({0}, {1}) to ({2}, {3})")
-            @CsvSource(delimiter = ',', textBlock =
-            """ 2,  0, 1,  0
+            @CsvSource(
+                delimiter = ',', textBlock =
+                """ 2,  0, 1,  0
                 -2, 0, -1, 0
                 0, 2, 0, 1
-                0, -2, 0, -1""")
+                0, -2, 0, -1"""
+            )
             fun `lateral step toward origin `(xS: Int, yS: Int, xE: Int, yE: Int) {
                 assertEquals(Position(xE, yE), Position(xS, yS).stepToward(origin))
             }
 
             @ParameterizedTest(name = "from ({0}, {1}) to ({2}, {3})")
-            @CsvSource(delimiter = ',', textBlock =
-            """ 2,  1, 1,  0
+            @CsvSource(
+                delimiter = ',', textBlock =
+                """ 2,  1, 1,  0
                 -2, 1, -1, 0
                 2, -1, 1, 0
                 -2, -1, -1, 0
                 1, 2, 0, 1
                 -1, 2, 0, 1
                 1, -2, 0, -1
-                -1, -2, 0, -1""")
+                -1, -2, 0, -1"""
+            )
             fun `diagonal step toward origin `(xS: Int, yS: Int, xE: Int, yE: Int) {
                 assertEquals(Position(xE, yE), Position(xS, yS).stepToward(origin))
             }
@@ -78,8 +84,9 @@ class Day9Test {
         inner class `touches(Position)` {
 
             @ParameterizedTest(name = "({0}, {1}) touches origin")
-            @CsvSource(delimiter = ',', textBlock =
-            """ 0, 0
+            @CsvSource(
+                delimiter = ',', textBlock =
+                """ 0, 0
                 0, 1
                 0, -1
                 1, 0
@@ -87,20 +94,23 @@ class Day9Test {
                 1, -1
                 -1, 0
                 -1, 1
-                -1, -1""")
+                -1, -1"""
+            )
             fun `touches origin`(x: Int, y: Int) =
                 assertTrue(Position(x, y).touches(origin))
 
             @ParameterizedTest(name = "({0}, {1}) does not touch origin")
-            @CsvSource(delimiter = ',', textBlock =
-            """ 0, 2
+            @CsvSource(
+                delimiter = ',', textBlock =
+                """ 0, 2
                 0, -2
                 2, 0
                 -2, 0
                 2, 1
                 2, -1
                 -2, 1
-                -2, -1""")
+                -2, -1"""
+            )
             fun `does not touch origin`(x: Int, y: Int) =
                 assertFalse(Position(x, y).touches(origin))
         }
@@ -112,34 +122,40 @@ class Day9Test {
         private val head = Knot(tail)
 
         @ParameterizedTest(name = "move({0}, {1}) -> ({2}, {3})")
-        @CsvSource(delimiter = ',', textBlock =
-        """R, 3, 3, 0
+        @CsvSource(
+            delimiter = ',', textBlock =
+            """R, 3, 3, 0
            U, 3, 0, 3
            L, 4, -4, 0
-           D, 4, 0, -4""")
+           D, 4, 0, -4"""
+        )
         fun `move n steps`(dir: Direction, steps: Int, xE: Int, yE: Int) {
             head.move(dir, steps)
             assertEquals(Position(xE, yE), head.currentPosition)
         }
 
         @ParameterizedTest(name = "move({0}) -> ({1}, {2})")
-        @CsvSource(delimiter = ',', textBlock =
-        """R, 1, 0
+        @CsvSource(
+            delimiter = ',', textBlock =
+            """R, 1, 0
            U, 0, 1
            L, -1, 0
-           D, 0, -1""")
+           D, 0, -1"""
+        )
         fun `move defaults to 1 step`(dir: Direction, xE: Int, yE: Int) {
             head.move(dir)
             assertEquals(Position(xE, yE), head.currentPosition)
         }
 
         @ParameterizedTest(name = "head moves {0}, tail ends at ({1}, {2})")
-        @CsvSource(delimiter = ',', textBlock =
-        """RRRRUU, 4, 1
+        @CsvSource(
+            delimiter = ',', textBlock =
+            """RRRRUU, 4, 1
            RRR, 2, 0
            R, 0, 0
            RRRUUDDLL, 2, 0
-           RRRUUDDL, 3, 1""")
+           RRRUUDDL, 3, 1"""
+        )
         fun `tail maintains contact with head`(dirs: String, xE: Int, yE: Int) {
             dirs.toCharArray().forEach { dir ->
                 head.move(Direction.valueOf(dir.toString()))
@@ -153,7 +169,8 @@ class Day9Test {
         @Test
         fun `two-knot rope`() {
             val rope = Rope()
-            rope.move("""
+            rope.move(
+                """
                 R 4
                 U 4
                 L 3
@@ -161,14 +178,16 @@ class Day9Test {
                 R 4
                 D 1
                 L 5
-                R 2""".trimIndent().lines())
+                R 2""".trimIndent().lines()
+            )
             assertEquals(13, rope.tail.visited.distinct().count())
         }
 
         @Test
         fun `ten-knot rope`() {
             val rope = Rope(10)
-            rope.move("""
+            rope.move(
+                """
                 R 5
                 U 8
                 L 8
@@ -176,7 +195,8 @@ class Day9Test {
                 R 17
                 D 10
                 L 25
-                U 20""".trimIndent().lines())
+                U 20""".trimIndent().lines()
+            )
             assertEquals(36, rope.tail.visited.distinct().count())
         }
     }
@@ -235,7 +255,7 @@ class Rope(knots: Int = 2) {
     private val head = (2..knots).fold(tail) { tail, _ -> Knot(tail) }
 
     fun move(steps: List<String>) = steps.forEach {
-        it.split(" ").let{ (dir, steps) ->
+        it.split(" ").let { (dir, steps) ->
             head.move(valueOf(dir), steps.toInt())
         }
     }
@@ -244,8 +264,8 @@ class Rope(knots: Int = 2) {
 class Day9alt(
     private val fileName: String,
     private val expected1: Int,
-    private val expected2: Int) : Solution
-{
+    private val expected2: Int
+) : Solution {
     override val day get() = 9
     override val source get() = "$fileName"
 
