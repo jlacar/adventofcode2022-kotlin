9f19380628be3ec44eaaab09aa85d712432ffd19
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2023-01-13 10:08:41 -0500

    Solved Day10 Part2 - crt display

--- untitled/src/main/kotlin/Day10.kt
+++ untitled/src/main/kotlin/Day10.kt
@@ -1,39 +1,60 @@
 class Day10(
     private val fileName: String,
     private val expected1: Int,
-    private val expected2: Int) : Solution
+    private val expected2: List<String>) : Solution
 {
     override val day: Int get() = 10
     override val source: String get() = "$fileName"
 
     private val program = InputReader(fileName).lines()
 
+    private val registerValues = mutableListOf(1).apply {
+        program.forEach { line ->
+            line.split(" ").let {
+                add(last())
+                if (it.size == 2) { add(last() + it[1].toInt()) }
+            }
+        }}.toList()
+
+
     private val sampleCycles = (20..220 step 40).toList()
 
     override fun part1() = Result(expected1, sampleSignalStrengths().sum())
 
-    private fun sampleSignalStrengths(): List<Int> = registerValues().mapIndexed { i, x ->
-        if (sampleCycles.contains(i + 1)) x * (i + 1) else 0
+    override fun part2() = Result(expected2, crtDisplay())
+
+    fun crtDisplay(): List<String> {
+        val crt = Array(6) { CharArray(40) { '.' } }
+        registerValues.take(240).forEachIndexed { i, center ->
+            val pixel = i % 40
+            if (pixel in center-1..center+1) { crt[i/40][pixel] = '#' }
+        }
+        return crt.map { it.joinToString(separator="") }
     }
 
-    private fun registerValues(): List<Int> =
-        mutableListOf(1).also { values ->
-            program.forEach { instruction ->
-                instruction.split(" ").let { parts ->
-                    values.add(values.last())
-                    if (parts.size == 2) {
-                        values.add(values.last() + parts[1].toInt())
-                    }
-                }
-            }
-        }.toList()
-
-    override fun part2() = PendingResult
+    private fun sampleSignalStrengths(): List<Int> = registerValues.mapIndexed { i, x ->
+        if (sampleCycles.contains(i + 1)) x * (i + 1) else 0
+    }
 }
 
 fun main() {
     Solution.report(
-        Day10("Day10-sample.txt", 13140, 0),
-        Day10("Day10.txt", 11960, 0)
+        Day10("Day10-sample.txt", 13140,
+            """##..##..##..##..##..##..##..##..##..##..
+              |###...###...###...###...###...###...###.
+              |####....####....####....####....####....
+              |#####.....#####.....#####.....#####.....
+              |######......######......######......####
+              |#######.......#######.......#######.....""".trimMargin().lines()),
+
+        Day10("Day10.txt", 11960,
+            """####...##..##..####.###...##..#....#..#.
+              |#.......#.#..#.#....#..#.#..#.#....#..#.
+              |###.....#.#....###..#..#.#....#....####.
+              |#.......#.#....#....###..#.##.#....#..#.
+              |#....#..#.#..#.#....#....#..#.#....#..#.
+              |####..##...##..#....#.....###.####.#..#.""".trimMargin().lines()).also {
+            it.crtDisplay().forEach(::println)  // EJCFPGLH
+        },
     )
 }
