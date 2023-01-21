469ef86d1ef2308ce98b55f0a2669d19dbfa1ec5
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2023-01-21 01:29:49 -0500

    Solved AoC 2021 Day1 - Sonar Sweep

---
+++ untitled/src/main/kotlin/aoc2021/Day1.kt
@@ -0,0 +1,34 @@
+package aoc2021
+
+import AocSolution
+import InputReader
+import shouldBe
+import solution
+
+class Day1(private val fileName: String) : AocSolution {
+
+    private val depths = InputReader(fileName).lines.map { it.toInt() }
+
+    override val description: String
+        get() = "AoC 2021 - Sonar Sweep ($fileName)"
+
+    override fun part1() = timesGoneDeeper(depths)
+
+    private fun timesGoneDeeper(depths: List<Int>) = depths.timesGoneDeeper()
+
+    override fun part2() = depths.windowed(3) { it.sum() }.timesGoneDeeper()
+
+}
+private fun List<Int>.timesGoneDeeper() =
+    this.windowed(2).count { (left, right) -> left < right }
+
+fun main() {
+    Day1("2021/Day1-sample.txt") solution {
+        part1() shouldBe 7
+        part2() shouldBe 5
+    }
+    Day1("2021/Day1.txt") solution {
+        part1() shouldBe 1390
+        part2() shouldBe 1457
+    }
+}
