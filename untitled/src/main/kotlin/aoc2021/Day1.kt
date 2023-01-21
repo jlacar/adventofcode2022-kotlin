dbdefda9a354589de31bc27e0690f7c61c1dc7c9
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2023-01-21 01:34:46 -0500

    Removed redundant timeGoneDeeper() in Day1, move that correct one back into Day1

--- untitled/src/main/kotlin/aoc2021/Day1.kt
+++ untitled/src/main/kotlin/aoc2021/Day1.kt
@@ -12,15 +12,13 @@ class Day1(private val fileName: String) : AocSolution {
     override val description: String
         get() = "AoC 2021 - Sonar Sweep ($fileName)"
 
-    override fun part1() = timesGoneDeeper(depths)
-
-    private fun timesGoneDeeper(depths: List<Int>) = depths.timesGoneDeeper()
+    override fun part1() = depths.timesGoneDeeper()
 
     override fun part2() = depths.windowed(3) { it.sum() }.timesGoneDeeper()
 
+    private fun List<Int>.timesGoneDeeper() =
+        this.windowed(2).count { (left, right) -> left < right }
 }
-private fun List<Int>.timesGoneDeeper() =
-    this.windowed(2).count { (left, right) -> left < right }
 
 fun main() {
     Day1("2021/Day1-sample.txt") solution {
